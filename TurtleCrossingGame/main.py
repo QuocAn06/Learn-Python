703bd595f4e2f4d783fd5aab55009d6ad39c740f
HP-PROBOOK\Mai Anh <lenguyenquocan116@gmail.com>
2022-08-21 15:56:52 +0700

    Revert "thêm car_manager.py vào dự án và chỉnh sửa main.py"
    This reverts commit db41dc19baf0654ad1c05a9f07ff4ec39e8a9640.

--- TurtleCrossingGame/main.py
+++ TurtleCrossingGame/main.py
@@ -1,5 +1,5 @@
 import time
-from turtle import Screen, Turtle
+from turtle import Screen
 from player import Player
 from car_manager import CarManager
 from scoreboard import Scoreboard
@@ -8,33 +8,7 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
-player = Player()
-car_manager = CarManager()
-
-# def car_move():
-#     new_x = car.xcor() - 10
-#     car.goto(new_x, car.ycor())
-
-
-screen.listen()
-screen.onkey(player.move, "Up")
-
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
-
-    car_manager.create_car()
-    car_manager.move_cars()
-
-    # Detect collision with car
-    for car in car_manager.all_cars:
-        if car.distance(player) < 22:
-            game_is_on = False
-
-    # Detect successful crossing
-    if player.is_at_finish_line():
-        player.go_to_start()
-        car_manager.level_up()
-
-screen.exitonclick()

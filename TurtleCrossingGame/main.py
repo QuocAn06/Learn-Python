302784fdc2c46f9d8bd85a9e4e97483a4884dc9a
HP-PROBOOK\Mai Anh <lenguyenquocan116@gmail.com>
2022-08-21 17:31:20 +0700

    Complete the project TurtleCrossingGame

--- TurtleCrossingGame/main.py
+++ TurtleCrossingGame/main.py
@@ -10,10 +10,7 @@ screen.tracer(0)
 
 player = Player()
 car_manager = CarManager()
-
-# def car_move():
-#     new_x = car.xcor() - 10
-#     car.goto(new_x, car.ycor())
+scoreboard = Scoreboard()
 
 
 screen.listen()
@@ -31,10 +28,12 @@ while game_is_on:
     for car in car_manager.all_cars:
         if car.distance(player) < 22:
             game_is_on = False
+            scoreboard.game_over()
 
     # Detect successful crossing
     if player.is_at_finish_line():
         player.go_to_start()
         car_manager.level_up()
+        scoreboard.increase_level()
 
 screen.exitonclick()

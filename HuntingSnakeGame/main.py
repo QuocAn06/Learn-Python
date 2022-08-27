dc4613ba91796a9f670202c12f3287ade2363784
HP-PROBOOK\Mai Anh <lenguyenquocan116@gmail.com>
2022-08-27 14:52:03 +0700

    HuntingSnakeGame start

---
+++ HuntingSnakeGame/main.py
@@ -0,0 +1,11 @@
+from turtle import Screen
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Hunting Snake Game")
+screen.tracer(0)
+
+
+
+screen.exitonclick()

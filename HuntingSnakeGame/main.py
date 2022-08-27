8150d8cd248f8391ccf4922ccc90180ebaf23e84
HP-PROBOOK\Mai Anh <lenguyenquocan116@gmail.com>
2022-08-27 15:26:12 +0700

    Add snake.py to project

--- HuntingSnakeGame/main.py
+++ HuntingSnakeGame/main.py
@@ -1,4 +1,10 @@
 from turtle import Screen
+from snake import Snake
+
+import time
+
+SPEED_OF_GAME = 0.1
+WALL_POSITION = 280
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -6,6 +12,24 @@ screen.bgcolor("black")
 screen.title("Hunting Snake Game")
 screen.tracer(0)
 
+snake = Snake()
 
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+is_game_on = True
+while is_game_on:
+    screen.update()
+    time.sleep(SPEED_OF_GAME)
+    snake.move()
+
+    # Detect collision with food.
+
+    # Detect collision with wall.
+
+    # Detect collision with tail.
 
 screen.exitonclick()

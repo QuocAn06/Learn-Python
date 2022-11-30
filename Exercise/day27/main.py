89dc2b672b6337cb0e245de20e80bb3964b88e27
HP-PROBOOK\Mai Anh <lenguyenquocan116@gmail.com>
2022-11-30 15:21:39 +0700

    30nov2022 - learn ablout tknter library

--- Exercise/day27/main.py
+++ Exercise/day27/main.py
@@ -1,1 +1,20 @@
 import tkinter
+
+window = tkinter.Tk()
+window.title("My First GUI Program")
+window.minsize(width=500, height=300)
+
+#Label
+my_label = tkinter.Label(text="I Am a Label", font=("Arial", 20, "bold"))
+my_label.pack(side="left")
+#my_label.pack(side="bottom")
+#my_label.pack(expand=True)
+
+
+
+window.mainloop()
+
+# def bar(spam, eggs):
+#     print(spam, eggs)
+#
+# bar(1, 2)

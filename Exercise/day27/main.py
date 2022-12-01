3bf394f8392e6561f1b4d96fd8a1ba43b410d7a3
HP-PROBOOK\Mai Anh <lenguyenquocan116@gmail.com>
2022-12-01 15:16:41 +0700

    01dec2022 - practice with button method

--- Exercise/day27/main.py
+++ Exercise/day27/main.py
@@ -1,17 +1,26 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
+window = Tk()
 window.title("My First GUI Program")
 window.minsize(width=500, height=300)
 
 #Label
-my_label = tkinter.Label(text="I Am a Label", font=("Arial", 20, "bold"))
-my_label.pack(side="left")
+my_label = Label(text="I Am a Label", font=("Arial", 20, "bold"))
+my_label.pack()
 #my_label.pack(side="bottom")
 #my_label.pack(expand=True)
 
 # my_label["text"] = "New text"
-my_label.config(text="New text")
+my_label.config(text="New text.")
+
+#Button
+
+def button_clicked():
+    # print("I got clicked.")
+    my_label.config(text="Button Got Clicked.")
+
+button = Button(text="Click Me", command=button_clicked)
+button.pack()
 
 window.mainloop()
 

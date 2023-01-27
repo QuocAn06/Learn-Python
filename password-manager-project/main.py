b3b169202875cf10908799186bfa69e9789ff0db
HP-PROBOOK\Mai Anh <lenguyenquocan116@gmail.com>
2023-01-27 23:58:45 +0700

    27jan2023 - 11:58 PM - import tkinter library. Create an interface window for the application.

--- password-manager-project/main.py
+++ password-manager-project/main.py
@@ -1,5 +1,14 @@
+from tkinter import *
+# ---------------------------- CONSTANTS ------------------------------- #
+NAME_OF_APP = "Password Manager"
+BLUE = "#cce5ff"
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title(NAME_OF_APP)
+window.config(padx=100, pady=50, bg=BLUE)
+
+window.mainloop()

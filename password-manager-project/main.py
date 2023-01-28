07729c08b30f4f4562ac7aec8e7c9dc1851c3456
HP-PROBOOK\Mai Anh <lenguyenquocan116@gmail.com>
2023-01-28 21:14:03 +0700

    28jan2023 - 09:11pm - Add buttons and adjust the position of each item with the gird function.

--- password-manager-project/main.py
+++ password-manager-project/main.py
@@ -1,7 +1,10 @@
 from tkinter import *
+
 # ---------------------------- CONSTANTS ------------------------------- #
 NAME_OF_APP = "Password Manager"
 BLUE = "#cce5ff"
+FONT_NAME = "Courier"
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
@@ -9,7 +12,7 @@ BLUE = "#cce5ff"
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title(NAME_OF_APP)
-window.config(padx=20, pady=20, bg=BLUE)
+window.config(padx=60, pady=60, bg=BLUE)
 
 canvas = Canvas(width=200, height=200, bg=BLUE, highlightthickness=0)
 lock_img = PhotoImage(file="./img/logo.png")
@@ -17,20 +20,30 @@ canvas.create_image(100, 100, image=lock_img)
 canvas.grid(row=0, column=1)
 
 # Labels
-website_label = Label(text="Website", bg=BLUE)
+website_label = Label(text="Website:", bg=BLUE, font=(FONT_NAME, 11, "bold"))
 website_label.grid(row=1, column=0)
 
-email_label = Label(text="Email/Username", bg=BLUE)
+email_label = Label(text="Email/Username:", bg=BLUE, font=(FONT_NAME, 11, "bold"))
 email_label.grid(row=2, column=0)
 
-password_label = Label(text="Password", bg=BLUE)
+password_label = Label(text="Password:", bg=BLUE, font=(FONT_NAME, 11, "bold"))
 password_label.grid(row=3, column=0)
 
 # Entries
-website_entry = Entry(width=35)
-website_entry.grid(row=1, column=1, columnspan=2)
+website_entry = Entry(width=46)
+website_entry.grid(row=1, column=1, columnspan=2, pady=2)
 
-email_entry = Entry(width=35)
-password_entry = Entry(width=21)
+email_entry = Entry(width=46)
+email_entry.grid(row=2, column=1, columnspan=2, pady=2)
+
+password_entry = Entry(width=27)
+password_entry.grid(row=3, column=1, pady=2)
+
+# Buttons
+generate_password_button = Button(text="Generate Password", font=(FONT_NAME, 8))
+generate_password_button.grid(row=3, column=2, pady=2)
+
+add_button = Button(text="Add", width=46)
+add_button.grid(row=4, column=1, columnspan=2)
 
 window.mainloop()

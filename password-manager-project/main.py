2e9c2133707026911ce4699006b1f648fe871480
HP-PROBOOK\Mai Anh <lenguyenquocan116@gmail.com>
2023-01-29 00:16:53 +0700

    29jan2023 - 12:15pm - Create and define the save function. Use this function for the purpose of saving the information entered on the user interface into the file data.txt

--- password-manager-project/main.py
+++ password-manager-project/main.py
@@ -4,10 +4,24 @@ from tkinter import *
 NAME_OF_APP = "Password Manager"
 BLUE = "#cce5ff"
 FONT_NAME = "Courier"
+EXAMPLE_EMAIL = "abc@gmail.com"
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def save():
+    website = website_entry.get()
+    email = email_entry.get()
+    password = password_entry.get()
+
+    with open("data.txt", "a") as data_file:
+        data_file.write(f"{website} | {email} | {password}\n")
+        website_entry.delete(0, END)
+        password_entry.delete(0, END)
+
+        if email != EXAMPLE_EMAIL:
+            email_entry.delete(0, END)
+            email_entry.insert(0, EXAMPLE_EMAIL)
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -36,7 +50,7 @@ website_entry.focus()
 
 email_entry = Entry(width=46)
 email_entry.grid(row=2, column=1, columnspan=2, padx=2, pady=2)
-email_entry.insert(0, "abc@gmail.com")
+email_entry.insert(0, EXAMPLE_EMAIL)
 
 password_entry = Entry(width=28)
 password_entry.grid(row=3, column=1, padx=3, pady=2)
@@ -45,7 +59,7 @@ password_entry.grid(row=3, column=1, padx=3, pady=2)
 generate_password_button = Button(text="Generate Password", font=(FONT_NAME, 8))
 generate_password_button.grid(row=3, column=2, pady=2)
 
-add_button = Button(text="Add", width=46)
+add_button = Button(text="Add", width=46, command=save)
 add_button.grid(row=4, column=1, columnspan=2)
 
 window.mainloop()

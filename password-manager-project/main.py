50656fc0bea515fba1056f07f070ea52035d744e
HP-PROBOOK\Mai Anh <lenguyenquocan116@gmail.com>
2023-01-29 22:27:04 +0700

    29jan2023 - 10:25pm - definition for generate_password function. Link the function to the button of the same name.

--- password-manager-project/main.py
+++ password-manager-project/main.py
@@ -1,41 +1,41 @@
 from tkinter import *
 from tkinter import messagebox
-import random
+from random import randint, choice, shuffle
 
 # ---------------------------- CONSTANTS ------------------------------- #
 NAME_OF_APP = "Password Manager"
 BLUE = "#cce5ff"
 FONT_NAME = "Courier"
 EXAMPLE_EMAIL = "abc@gmail.com"
+LETTERS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
+           'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+           's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A',
+           'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+           'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
+           'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+NUMBERS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+SYMBOLS = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+def generate_password():
+    nr_letters = randint(8, 10)
+    nr_symbols = randint(2, 4)
+    nr_numbers = randint(2, 4)
 
-nr_letters = random.randint(8, 10)
-nr_symbols = random.randint(2, 4)
-nr_numbers = random.randint(2, 4)
+    password_letters = [choice(LETTERS) for _ in range(nr_letters)]
+    password_symbols = [choice(SYMBOLS) for _ in range(nr_symbols)]
+    password_numbers = [choice(NUMBERS) for _ in range(nr_numbers)]
 
-password_list = []
+    password_list = password_letters + password_symbols + password_numbers
+    shuffle(password_list)
 
-for char in range(nr_letters):
-  password_list.append(random.choice(letters))
+    password = "".join(password_list)
 
-for char in range(nr_symbols):
-  password_list += random.choice(symbols)
-
-for char in range(nr_numbers):
-  password_list += random.choice(numbers)
-
-random.shuffle(password_list)
-
-password = ""
-for char in password_list:
-  password += char
-
-print(f"Your password is: {password}")
+    if len(password_entry.get()) != 0:
+        password_entry.delete(0, END)
+        
+    password_entry.insert(0, password)
 
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
@@ -94,7 +94,7 @@ password_entry = Entry(width=28)
 password_entry.grid(row=3, column=1, padx=3, pady=2)
 
 # Buttons
-generate_password_button = Button(text="Generate Password", font=(FONT_NAME, 8))
+generate_password_button = Button(text="Generate Password", font=(FONT_NAME, 8), command=generate_password)
 generate_password_button.grid(row=3, column=2, pady=2)
 
 add_button = Button(text="Add", width=46, command=save)

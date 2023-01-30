58eed2de954dea02786257e11f03f19545b20d3d
HP-PROBOOK\Mai Anh <lenguyenquocan116@gmail.com>
2023-01-30 23:51:25 +0700

    30jan2023 - 11:45pm - added exception handling for previously created nato-alphabet project. Save as another project with the name nato-alphabet-02.

--- nato-alphabet-02/main.py
+++ nato-alphabet-02/main.py
@@ -1,14 +1,20 @@
 import pandas
 
 data = pandas.read_csv("./nato_phonetic_alphabet.csv")
-print(data.to_dict())
 
-# TODO 1. Create a dictionary in this format:
 phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
-# print(phonetic_dict)
+print(phonetic_dict)
 
-# TODO 2. Create a list of the phonetic code words from a word that the user input:
-word = input("Enter the word: ").upper()
-output_list = [phonetic_dict[letter] for letter in word]
-print(output_list)
 
+def nato_alphabet():
+    try:
+        word = input("Enter the word: ").upper()
+        output_list = [phonetic_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        nato_alphabet()
+    else:
+        print(output_list)
+
+
+nato_alphabet()

282567473e8144685e3968061c92b1d9ea2a9bb9
HP-PROBOOK\Mai Anh <lenguyenquocan116@gmail.com>
2023-02-01 21:33:49 +0700

    01feb2023 - 09:28pm - Change the data type from text to json. Create file data.json.

--- password-manager-project/main.py
+++ password-manager-project/main.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import messagebox
 from random import randint, choice, shuffle
 import pyperclip
+import json
 
 # ---------------------------- CONSTANTS ------------------------------- #
 NAME_OF_APP = "Password Manager"
@@ -44,22 +45,25 @@ def save():
     website = website_entry.get()
     email = email_entry.get()
     password = password_entry.get()
+    new_data = {
+        website:{
+            "email": email,
+            "password": password
+        }
+    }
 
     if (len(website) == 0) or (len(password) == 0) or (len(email) == 0):
         messagebox.askquestion(title="Oops", message="Please don't leave any fields empty!")
     else:
-        is_ok = messagebox.askokcancel(title=website, message=f"There are the details entered: \nEmail: {email}"
-                                                              f"\nPassword: {password} \nIs it ok to save?")
+        with open("data.json", "w") as data_file:
+            json.dump(new_data, data_file, indent=4)
 
-        if is_ok:
-            with open("data.txt", "a") as data_file:
-                data_file.write(f"{website} | {email} | {password}\n")
-                website_entry.delete(0, END)
-                password_entry.delete(0, END)
+            website_entry.delete(0, END)
+            password_entry.delete(0, END)
 
-                if email != EXAMPLE_EMAIL:
-                    email_entry.delete(0, END)
-                    email_entry.insert(0, EXAMPLE_EMAIL)
+            if email != EXAMPLE_EMAIL:
+                email_entry.delete(0, END)
+                email_entry.insert(0, EXAMPLE_EMAIL)
 
 
 # ---------------------------- UI SETUP ------------------------------- #

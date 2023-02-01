1679906f23a9dc94fdbc8c094b5c739e164f59c0
HP-PROBOOK\Mai Anh <lenguyenquocan116@gmail.com>
2023-02-01 22:01:39 +0700

    01feb2023 - 10:01pm - create a function that allows you to save more json data into the data.json file without overwriting the old data.

--- password-manager-project/main.py
+++ password-manager-project/main.py
@@ -46,7 +46,7 @@ def save():
     email = email_entry.get()
     password = password_entry.get()
     new_data = {
-        website:{
+        website: {
             "email": email,
             "password": password
         }
@@ -55,9 +55,22 @@ def save():
     if (len(website) == 0) or (len(password) == 0) or (len(email) == 0):
         messagebox.askquestion(title="Oops", message="Please don't leave any fields empty!")
     else:
-        with open("data.json", "w") as data_file:
-            json.dump(new_data, data_file, indent=4)
+        try:
+            with open("data.json", "r") as data_file:
+                # Reading old data
+                data = json.load(data_file)
+        except FileNotFoundError:
+            with open("data.json", "w") as data_file:
+                json.dump(new_data, data_file, indent=4)
+        else:
+            # Updating old data with new data
+            data.update(new_data)
 
+            with open("data.json", "w") as data_file:
+                # Saving update data
+                json.dump(data, data_file, indent=4)
+
+        finally:
             website_entry.delete(0, END)
             password_entry.delete(0, END)
 

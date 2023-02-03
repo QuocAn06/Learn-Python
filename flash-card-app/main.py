0cb0cc741c68bef5521d641219752c1cb411754d
HP-PROBOOK\Mai Anh <lenguyenquocan116@gmail.com>
2023-02-03 19:24:50 +0700

    03feb2023 - 07:22pm - Set up the foundation for building flash card applications.

---
+++ flash-card-app/main.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+# ---------------------------- CONSTANTS ------------------------------- #
+BACKGROUND_COLOR = "#B1DDC6"
+NAME_OF_APP = "Flashy"
+
+# ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title(NAME_OF_APP)
+window.config(padx=60, pady=60, bg=BACKGROUND_COLOR)
+
+window.mainloop()

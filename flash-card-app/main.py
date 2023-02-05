e42514ae7a0ba8b116c1674cca58d30a7a63ac2f
HP-PROBOOK\Mai Anh <lenguyenquocan116@gmail.com>
2023-02-05 19:45:11 +0700

    05feb2023 - 07:42pm - call the next_card function before the mainloop function to avoid generating information for the first flashcard.

--- flash-card-app/main.py
+++ flash-card-app/main.py
@@ -14,7 +14,7 @@ to_learn = data.to_dict(orient="records")
 def next_card():
     current_card = random.choice(to_learn)
     canvas.itemconfig(card_title, text="French")
-    canvas.itemconfig(card_word, text=current_card["French"])
+    canvas.itemconfig(card_word , text=current_card["French"])
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -25,8 +25,8 @@ window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 canvas = Canvas(width=800, height=526)
 lock_img = PhotoImage(file="./img/card_front.png")
 canvas.create_image(400, 263, image=lock_img)
-card_title = canvas.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"))
-card_word = canvas.create_text(400, 263, text="word", font=("Ariel", 60, "bold"))
+card_title = canvas.create_text(400, 150, text="", font=("Ariel", 40, "italic"))
+card_word = canvas.create_text(400, 263, text="", font=("Ariel", 60, "bold"))
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.grid(row=0, column=0, columnspan=2)
 
@@ -40,5 +40,7 @@ check_img = PhotoImage(file="./img/right.png")
 know_button = Button(image=check_img)
 know_button.config(bg=BACKGROUND_COLOR, highlightthickness=0, command=next_card)
 know_button.grid(row=1, column=1)
+
+next_card()
 
 window.mainloop()

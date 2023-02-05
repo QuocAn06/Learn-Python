bc5e054b734fba674e7491d829674b4c80b5bfac
HP-PROBOOK\Mai Anh <lenguyenquocan116@gmail.com>
2023-02-05 21:32:46 +0700

    05feb2023 - 09:32pm - create a function is_known used to mark and remove words that you have learned

--- flash-card-app/main.py
+++ flash-card-app/main.py
@@ -6,19 +6,27 @@ import random
 BACKGROUND_COLOR = "#B1DDC6"
 NAME_OF_APP = "Flashy"
 
-data = pandas.read_csv("data/french_words.csv")
-to_learn = data.to_dict(orient="records")
+to_learn = {}
 current_card = {}
+
+try:
+    data = pandas.read_csv("./data/words_to_learn.csv")
+except FileNotFoundError:
+    original_data = pandas.read_csv("./data/french_words.csv")
+    to_learn = original_data.to_dict(orient="records")
+else:
+    to_learn = data.to_dict(orient="records")
 
 
 # ---------------------------- NEXT CARD ------------------------------- #
 def next_card():
-    global current_card
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
     current_card = random.choice(to_learn)
     canvas.itemconfig(card_title, text="French", fill="black")
     canvas.itemconfig(card_word, text=current_card["French"], fill="black")
     canvas.itemconfig(card_background, image=card_font_img)
-    window.after(3000, func=flip_card)
+    flip_timer = window.after(3000, func=flip_card)
 
 
 # ---------------------------- FLIP CARD ------------------------------- #
@@ -27,10 +35,23 @@ def flip_card():
     canvas.itemconfig(card_word, text=current_card["English"], fill="white")
     canvas.itemconfig(card_background, image=card_back_img)
 
+
+# ---------------------------- NEXT CARD ------------------------------- #
+def is_known():
+    global data
+    to_learn.remove(current_card)
+    data = pandas.DataFrame(to_learn)
+    data.to_csv("./data/words_to_learn.csv", index=False)
+
+    next_card()
+
+
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title(NAME_OF_APP)
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+
+flip_timer = window.after(3000, func=flip_card)
 
 canvas = Canvas(width=800, height=526)
 card_font_img = PhotoImage(file="./img/card_front.png")
@@ -49,7 +70,7 @@ unknow_button.grid(row=1, column=0)
 
 check_img = PhotoImage(file="./img/right.png")
 know_button = Button(image=check_img)
-know_button.config(bg=BACKGROUND_COLOR, highlightthickness=0, command=next_card)
+know_button.config(bg=BACKGROUND_COLOR, highlightthickness=0, command=is_known)
 know_button.grid(row=1, column=1)
 
 next_card()

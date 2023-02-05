84e63555dbfb94e66c2c815f2c7294b2e93095cc
HP-PROBOOK\Mai Anh <lenguyenquocan116@gmail.com>
2023-02-05 17:26:39 +0700

    05feb2023 - 05:24pm - Create next_card function to convert between words. In which, the choice of random is used to choose words from the collected data.

--- flash-card-app/main.py
+++ flash-card-app/main.py
@@ -13,6 +13,8 @@ to_learn = data.to_dict(orient="records")
 # ---------------------------- NEXT CARD ------------------------------- #
 def next_card():
     current_card = random.choice(to_learn)
+    canvas.itemconfig(card_title, text="French")
+    canvas.itemconfig(card_word, text=current_card["French"])
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -23,8 +25,8 @@ window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 canvas = Canvas(width=800, height=526)
 lock_img = PhotoImage(file="./img/card_front.png")
 canvas.create_image(400, 263, image=lock_img)
-canvas.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"))
-canvas.create_text(400, 263, text="word", font=("Ariel", 60, "bold"))
+card_title = canvas.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"))
+card_word = canvas.create_text(400, 263, text="word", font=("Ariel", 60, "bold"))
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.grid(row=0, column=0, columnspan=2)
 

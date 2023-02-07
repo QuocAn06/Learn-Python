8ccf5a2446d6b924afacab5e003723fedf6a6f84
HP-PROBOOK\Mai Anh <lenguyenquocan116@gmail.com>
2023-02-07 23:27:28 +0700

    07feb2023 - 11:27pm - Use the smtplib library to send mail. when using the smtplib.SMTP function, you must add the value variable port to it.

--- birthday-wisher/main.py
+++ birthday-wisher/main.py
@@ -1,3 +1,10 @@
 import smtplib
 
-connection = 
+my_email = "sorry8699@gmail.com"
+my_password = "zuuxlcqvdwtnvrtx"
+
+connection = smtplib.SMTP("smtp.gmail.com", port=587)
+connection.starttls()
+connection.login(user=my_email, password=my_password)
+connection.sendmail(from_addr=my_email, to_addrs="lenguyenquocan116@gmail.com", msg="Hello")
+connection.close()

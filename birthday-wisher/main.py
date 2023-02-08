93911c45a7d4ec5f589bc02da0f16a786ea6761f
HP-PROBOOK\Mai Anh <lenguyenquocan116@gmail.com>
2023-02-08 23:44:07 +0700

    08feb2023 - 11:43pm - add Subject and body for email

--- birthday-wisher/main.py
+++ birthday-wisher/main.py
@@ -2,9 +2,13 @@ import smtplib
 
 my_email = "sorry8699@gmail.com"
 my_password = "zuuxlcqvdwtnvrtx"
+partner_email = "lenguyenquocan116@gmail.com"
+
 
 connection = smtplib.SMTP("smtp.gmail.com", port=587)
 connection.starttls()
 connection.login(user=my_email, password=my_password)
-connection.sendmail(from_addr=my_email, to_addrs="lenguyenquocan116@gmail.com", msg="Hello")
+connection.sendmail(from_addr=my_email,
+                    to_addrs=partner_email,
+                    msg="Subject:Hello\n\nThis is the body of my email.")
 connection.close()

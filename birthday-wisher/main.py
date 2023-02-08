a45206cb7b8b03a938596331d967c374b4d0b5b5
HP-PROBOOK\Mai Anh <lenguyenquocan116@gmail.com>
2023-02-08 23:53:50 +0700

    08feb2023 - 11:52pm - update more methods to perform email sending.

--- birthday-wisher/main.py
+++ birthday-wisher/main.py
@@ -1,14 +1,19 @@
 import smtplib
 
+EMAIL_SMTP = "smtp.gmail.com"
+EMAIL_PORT = 587
+
 my_email = "sorry8699@gmail.com"
 my_password = "zuuxlcqvdwtnvrtx"
 partner_email = "lenguyenquocan116@gmail.com"
 
 
-connection = smtplib.SMTP("smtp.gmail.com", port=587)
-connection.starttls()
-connection.login(user=my_email, password=my_password)
-connection.sendmail(from_addr=my_email,
-                    to_addrs=partner_email,
-                    msg="Subject:Hello\n\nThis is the body of my email.")
-connection.close()
+with smtplib.SMTP(EMAIL_SMTP, port=EMAIL_PORT) as connection:
+    connection.starttls()
+    connection.login(user=my_email, password=my_password)
+    connection.sendmail(
+        from_addr=my_email,
+        to_addrs=partner_email,
+        msg="Subject:Hello\n\nThis is the body of my email."
+    )
+

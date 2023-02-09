65c2307cf0ed7cfb540e55847f13bf0959e8d110
HP-PROBOOK\Mai Anh <lenguyenquocan116@gmail.com>
2023-02-10 00:07:08 +0700

    10feb2023 - 12:06pm - Send Motivational Quotes on Mondays via Email

--- birthday-wisher/main.py
+++ birthday-wisher/main.py
@@ -1,19 +1,69 @@
 import smtplib
+import datetime as dt
+import random as rd
 
-EMAIL_SMTP = "smtp.gmail.com"
-EMAIL_PORT = 587
+DOMAIN_SMTP = "smtp.gmail.com"
+DOMAIN_PORT = 587
+MY_EMAIL = "sorry8699@gmail.com"
+MY_PASSWORD = "zuuxlcqvdwtnvrtx"
+PARTNER_EMAIL = "lenguyenquocan116@gmail.com"
+EMAIL_SUBJECT = "Monday Motivation"
 
-my_email = "sorry8699@gmail.com"
-my_password = "zuuxlcqvdwtnvrtx"
-partner_email = "lenguyenquocan116@gmail.com"
+now = dt.datetime.now()
+weekday = now.weekday()
+
+if weekday == 4:
+    with open("./data/quotes.txt") as quote_file:
+        all_quotes = quote_file.readlines()
+        quote = rd.choice(all_quotes)
+
+    print(quote)
+    with smtplib.SMTP(DOMAIN_SMTP, DOMAIN_PORT) as connection:
+        connection.starttls()
+        connection.login(MY_EMAIL, MY_PASSWORD)
+        connection.sendmail(
+            from_addr=MY_EMAIL,
+            to_addrs=PARTNER_EMAIL,
+            msg=f"Subject:{EMAIL_SUBJECT}\n\n{quote}"
+        )
 
 
-with smtplib.SMTP(EMAIL_SMTP, port=EMAIL_PORT) as connection:
-    connection.starttls()
-    connection.login(user=my_email, password=my_password)
-    connection.sendmail(
-        from_addr=my_email,
-        to_addrs=partner_email,
-        msg="Subject:Hello\n\nThis is the body of my email."
-    )
 
+
+
+
+
+
+
+
+
+
+# import smtplib
+# import datetime as dt
+#
+# # # ------------------- CONSTANTS -------------------
+# # DOMAIN_SMTP = "smtp.gmail.com"
+# # DOMAIN_PORT = 587
+# #
+# # MY_EMAIL = "sorry8699@gmail.com"
+# # MY_PASSWORD = "zuuxlcqvdwtnvrtx"
+# # PARTNER_EMAIL = "lenguyenquocan116@gmail.com"
+# #
+# #
+# # # ---------------- CONNECT EMAIL DOMAIN ----------------
+# # with smtplib.SMTP(EMAIL_SMTP, port=EMAIL_PORT) as connection:
+# #     connection.starttls()
+# #     connection.login(user=MY_EMAIL, password=MY_PASSWORD)
+# #     connection.sendmail(
+# #         from_addr=MY_EMAIL,
+# #         to_addrs=PARTNER_EMAIL,
+# #         msg="Subject:Hello\n\nThis is the body of my email."
+# #     )
+#
+# now = dt.datetime.now()
+# day = now.day
+# year = now.year
+#
+# date_of_birth = dt.datetime(year=1997, month=6, day=11)
+#
+# print(date_of_birth)

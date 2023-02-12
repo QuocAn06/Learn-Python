7dabe963fa448dcf78e6bfb8d63c7dba6b21cc2d
HP-PROBOOK\Mai Anh <lenguyenquocan116@gmail.com>
2023-02-12 22:20:11 +0700

    12feb2023 - 10:20pm - compare today's date and birthday data. If there are duplicate data, you can randomly choose any letter.txt file.

--- birthday-wisher/main.py
+++ birthday-wisher/main.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 import pandas
+import random
 
 today = datetime.now()
 today_tuple = (today.month, today.day)
@@ -8,4 +9,6 @@ data = pandas.read_csv("./data/birthdays.csv")
 birthday_dict = {
     (data_row["month"], data_row["day"]): data_row for (index, data_row) in data.iterrows()
 }
-
+if today_tuple in birthday_dict:
+    file_path = f"./data/letter_templates/letter_{random.randint(1,3)}.txt"
+    

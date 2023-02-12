084d8effe7e3ab4f577ecc39db990a6c1fcfb9d2
HP-PROBOOK\Mai Anh <lenguyenquocan116@gmail.com>
2023-02-12 21:28:30 +0700

    12feb2023 - 09:18pm - get information about birthday in file birthday.csv

--- birthday-wisher/main.py
+++ birthday-wisher/main.py
@@ -1,4 +1,11 @@
 from datetime import datetime
+import pandas
 
 today = datetime.now()
 today_tuple = (today.month, today.day)
+
+data = pandas.read_csv("./data/birthdays.csv")
+birthday_dict = {
+    (data_row["month"], data_row["day"]): data_row for (index, data_row) in data.iterrows()
+}
+

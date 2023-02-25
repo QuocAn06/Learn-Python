868986aaeb1b1162ee7c6654c375d32b63d337ea
HP-PROBOOK\Mai Anh <lenguyenquocan116@gmail.com>
2023-02-26 00:46:12 +0700

    feat: initialize 2 buttons True / False

--- quizzler-app/ui.py
+++ quizzler-app/ui.py
@@ -23,4 +23,12 @@ class QuizInterface:
         )
         self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
 
+        true_image = PhotoImage(file="./img/true.png")
+        self.true_button = Button(image=true_image, highlightthickness=0)
+        self.true_button.grid(row=2, column=0)
+
+        false_image = PhotoImage(file="./img/false.png")
+        self.false_button = Button(image=false_image, highlightthickness=0)
+        self.false_button.grid(row=2, column=1)
+
         self.window.mainloop()

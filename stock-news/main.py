0fe6e5283e7b8d72e6b772212e92f1ca7ebc0493
HP-PROBOOK\Mai Anh <lenguyenquocan116@gmail.com>
2023-03-10 17:13:02 +0700

    build: Send each article as a separate message via Twilio.

--- stock-news/main.py
+++ stock-news/main.py
@@ -1,4 +1,3 @@
-import os
 from requests import *
 from twilio.rest import Client
 
@@ -10,13 +9,13 @@ NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 
 STOCK_API_KEY = "S9PSG0DWSPLKEHVG"
 
-ACCOUNT_SID = "AC7a7680d5ab5701ab2c001438e3969cb9"
-AUTH_TOKEN = "5ed5bf2d83d4ea3856803fb1e64c6100"
+TWILIO_SID = "AC7a7680d5ab5701ab2c001438e3969cb9"
+TWILIO_AUTH_TOKEN = "67cd8f6881e3c9569e44e62ce47a385d"
 
     ## STEP 1: Use https://www.alphavantage.co/documentation/#daily
 # When stock price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
 
-#TODO 1. - Get yesterday's closing stock price. Hint: You can perform list comprehensions on Python dictionaries. e.g. [new_value for (key, value) in dictionary.items()]
+#Get yesterday's closing stock price. Hint: You can perform list comprehensions on Python dictionaries. e.g. [new_value for (key, value) in dictionary.items()]
 stock_params = {
     "function": "TIME_SERIES_DAILY_ADJUSTED",
     "symbol": STOCK_NAME,
@@ -30,17 +29,22 @@ yesterday_data = stock_data_list[0]
 yesterday_closing_price = yesterday_data["4. close"]
 
 
-#TODO 2. - Get the day before yesterday's closing stock price
+#Get the day before yesterday's closing stock price
 day_before_yesterday_data = stock_data_list[1]
 day_before_yesterday_closing_price = day_before_yesterday_data["4. close"]
 
-#TODO 3. - Find the positive difference between 1 and 2. e.g. 40 - 20 = -20, but the positive difference is 20. Hint: https://www.w3schools.com/python/ref_func_abs.asp
+#Find the positive difference between 1 and 2. e.g. 40 - 20 = -20, but the positive difference is 20. Hint: https://www.w3schools.com/python/ref_func_abs.asp
 pos_diff = abs(float(yesterday_closing_price) - float(day_before_yesterday_closing_price))
+up_down = None
+if pos_diff > 0:
+    up_down = "🔺"
+else:
+    up_down = "🔻"
 
-#TODO 4. - Work out the percentage difference in price between closing price yesterday and closing price the day before yesterday.
+#Work out the percentage difference in price between closing price yesterday and closing price the day before yesterday.
 per_diff = (pos_diff/float(yesterday_closing_price))*100
 
-#TODO 5. - If TODO4 percentage is greater than 5 then print("Get News").
+#If TODO4 percentage is greater than 5 then print("Get News").
 if per_diff > 2:
     print("Get News")
 
@@ -52,21 +56,28 @@ if per_diff > 2:
         "q": COMPANY_NAME,
     }
 
-    #TODO 6. - Instead of printing ("Get News"), use the News API to get articles related to the COMPANY_NAME.
+    #Instead of printing ("Get News"), use the News API to get articles related to the COMPANY_NAME.
     news_response = get(url=NEWS_ENDPOINT, params=news_params)
     articles = news_response.json()["articles"]
 
-    #TODO 7. - Use Python slice operator to create a list that contains the first 3 articles. Hint: https://stackoverflow.com/questions/509211/understanding-slice-notation
+    #Use Python slice operator to create a list that contains the first 3 articles. Hint: https://stackoverflow.com/questions/509211/understanding-slice-notation
     three_articles = articles[:3]
     print(three_articles)
 
     ## STEP 3: Use twilio.com/docs/sms/quickstart/python
     #to send a separate message with each article's title and description to your phone number.
+    client = Client(TWILIO_SID, TWILIO_AUTH_TOKEN)
 
-#TODO 8. - Create a new list of the first 3 article's headline and description using list comprehension.
+    #Create a new list of the first 3 article's headline and description using list comprehension.
+    formatted_articles = [f"{STOCK_NAME}: {up_down}{diff_percent}%\nHeadline: {article['title']}. \nBrief: {article['description']}" for article in three_articles]
 
-#TODO 9. - Send each article as a separate message via Twilio.
-
+    #TODO 9. - Send each article as a separate message via Twilio.
+    for article in formatted_articles:
+        message = client.messages.create(
+            body=article,
+            from_="+15673444946",
+            to="+84332101988"
+        )
 
 
 #Optional TODO: Format the message like this:

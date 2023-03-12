15fa896b33516211c868f53962cbdaefb8080f88
HP-PROBOOK\Mai Anh <lenguyenquocan116@gmail.com>
2023-03-12 21:18:52 +0700

    feat: complete the creation of an account on the pixela website

--- habit-tracker/main.py
+++ habit-tracker/main.py
@@ -1,3 +1,13 @@
 import requests
 
-pixela_endpoint = ""
+pixela_endpoint = "https://pixe.la/v1/users"
+
+user_params = {
+    "token": "oDo361Itx4b5687rDgVB0je9xV",
+    "username": "lenguyenquocan116",
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes",
+}
+
+response = requests.post(url=pixela_endpoint, json=user_params)
+print(response.text)

c369fe811a53dbae504200dd6d6aac2b9e00445b
HP-PROBOOK\Mai Anh <lenguyenquocan116@gmail.com>
2023-03-19 00:05:42 +0700

    build: add sheety_endpoint and set up to the POST request to sheety

--- exercise_tracking/main.py
+++ exercise_tracking/main.py
@@ -1,4 +1,5 @@
 import requests
+from datetime import datetime
 
 GENDER = "male"
 WEIGHT_KG = 89
@@ -9,8 +10,15 @@ APP_ID = "802368ef"
 APP_KEY = "2ba4900dbd7d46f2e744c7fde87ea238"
 
 exercise_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
+sheety_endpoint = "https://api.sheety.co/43f0a7eb357595513b77fb2a238ae284/workoutTracking/workouts"
 
 exercise_text = input("Tell me which exercise you did: ")
+
+headers = {
+    "x-app-id": APP_ID,
+    "x-app-key": APP_KEY,
+    "Content-Type": "application/json"
+}
 
 parameters = {
     "query": exercise_text,
@@ -20,3 +28,17 @@ parameters = {
     "age": AGE
 }
 
+response = requests.post(
+    url=exercise_endpoint,
+    json=parameters,
+    headers=headers
+)
+
+result = response.json()
+
+today_date = datetime.now().strftime("%d%m%Y")
+now_time = datetime.now().strftime("%X")
+
+
+
+print(response.text)
